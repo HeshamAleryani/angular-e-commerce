897499da7bb1bfdc6b465af08c1322b3a5ac762f
HeshamAleryani <heshaker@gmail.com>
2022-07-08 17:43:30 +0300

    Adding submit issue form + styling

--- src/app/components/issue/issue.component.ts
+++ src/app/components/issue/issue.component.ts
@@ -1,15 +1,55 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var $: any;
+
 @Component({
   selector: 'app-issue',
   templateUrl: './issue.component.html',
   styleUrls: ['./issue.component.css'],
 })
 export class IssueComponent implements OnInit {
+  isSuccess: boolean = false;
   constructor() {}
 
-  ngOnInit(): void {}
-  submit() {
-    console.log('form submited');
+  ngOnInit(): void {
+    $('form').form({
+      on: 'blur',
+      fields: {
+        username: {
+          identifier: 'paragraph',
+          rules: [
+            {
+              type: 'empty',
+              prompt: 'Please enter the message ',
+            },
+          ],
+        },
+        name: {
+          identifier: 'name',
+          rules: [
+            {
+              type: 'empty',
+              prompt: 'Please enter your name',
+            },
+          ],
+        },
+
+        il: {
+          identifier: 'email',
+          rules: [
+            {
+              type: 'email',
+              prompt: 'Please enter a valid e-mail',
+            },
+          ],
+        },
+      },
+      onSuccess: (event: SubmitEvent, fields: Array<any>) => {
+        event.preventDefault();
+        console.log(fields);
+        this.isSuccess = true;
+        $('form').form('reset');
+      },
+    });
   }
 }
